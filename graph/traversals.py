4e8f05c4b4e2e296153e141047a1a2485458bcdb
ayoub <ayoubousbia02@gmail.com>
2024-01-01 13:05:23 +0100

    add BFS, DFS traversals

---
+++ graph/traversals.py
@@ -0,0 +1,33 @@
+from collections import deque
+
+
+def depth_first_print(graph, start):
+    stack = [start]
+    while len(stack) > 0:
+        current = stack[-1]
+        print(current)
+        stack.pop()
+        for neighbor in graph[current]:
+            stack.append(neighbor)
+
+
+def depth_first_print_recursive(graph, current):
+    print(current)
+    for neighbor in graph[current]:
+        depth_first_print_recursive(graph, neighbor)
+
+
+def breadth_first_print(graph, start):
+    queue = deque([start])
+    while len(queue) > 0:
+        current = queue[0]
+        print(current)
+        queue.popleft()
+        for neighbor in graph[current]:
+            queue.append(neighbor)
+
+
+graph = {"a": ["b", "c"], "b": ["d"], "c": ["e"], "d": ["f"], "e": [], "f": []}
+
+# depth_first_print(graph, "a")
+breadth_first_print(graph, "a")
